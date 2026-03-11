e1bbd1afb9104ff8be058739d2aac181d956d4d3
mrred <zarubinr85@gmail.com>
2026-03-11 23:02:17 +0700

    +Command Parser realization

---
+++ TerminalCommandMenu/CommandParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TerminalCommandMenu.Abstractions;
+
+namespace TerminalCommandMenu
+{
+    public class CommandParser : ICommandParser
+    {
+        public (string Command, string[] arguments) Parse(string input)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace("Input string is null or white space", input);
+
+            string[] parseString = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (parseString.Length == 0) throw new ArgumentException("Input string is incorrect", input);
+
+            return (parseString[0], parseString.Length > 1 ? parseString[1..] : []);
+        }
+    }
+}
