d0c060eb875e1bce9f2bf831384122b3171efd20
mrred <zarubinr85@gmail.com>
2026-03-11 20:14:40 +0700

    Добавьте файлы проекта.

---
+++ TerminalCommandMenu/Abstractions/ITerminal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerminalCommandMenu.Abstractions
+{
+    public interface ITerminal
+    {
+        void Write(string text);
+        
+        string Read();
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
