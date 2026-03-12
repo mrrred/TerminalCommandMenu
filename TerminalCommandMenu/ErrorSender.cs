830a043f9cc7f7ad2265bee61ee0de39e47ffeb4
mrred <zarubinr85@gmail.com>
2026-03-13 02:00:37 +0700

    +Error sender

---
+++ TerminalCommandMenu/ErrorSender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TerminalCommandMenu.Abstractions;
+
+namespace TerminalCommandMenu
+{
+    public class ErrorSender : IErrorSender
+    {
+        private IWriteble _writer;
+
+        public ErrorSender(IWriteble writer)
+        {
+            _writer = writer;
+        }
+
+        public void NotifyOnError(string message)
+        {
+            _writer.Write(message);
+        }
+    }
+}
